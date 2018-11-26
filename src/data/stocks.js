8c9408530c2b87030a946cd4df9234558e4e4fd0
Shivam Shrestha <3rdsty4bl00d@gmail.com>
2018-11-26 14:06:44 +0545

    changed data stocks

--- src/data/stocks.js
+++ src/data/stocks.js
@@ -2,7 +2,7 @@ import stocks from '../data/stocks'
 
 export default [
     { id: 1, name: 'BMW', price: 100 },
-    { id: 2, name: 'GOOGLE', price: 200 },
-    { id: 3, name: 'APPLE', price: 250 },
-    { id: 4, name: 'TWITTER', price: 8 }
+    { id: 2, name: 'Google', price: 200 },
+    { id: 3, name: 'Apple', price: 250 },
+    { id: 4, name: 'Twitter', price: 8 }
 ];
